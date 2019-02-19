e7bc5b2a039ca39891e3964c3f3d9d240ffd742e
MrMedici <mr.medici@foxmail.com>
2019-02-19 15:39:07 +0800

    commit fair lock in thread

---
+++ src/main/kotlin/chapter2/lesson6/FairLockInThread.kt
@@ -0,0 +1,59 @@
+package chapter2.lesson6
+
+import java.util.concurrent.locks.ReentrantLock
+
+private class PrintQueue{
+    private val queueLock = ReentrantLock(true)
+
+    fun printJob(document:Any){
+        queueLock.lock()
+
+        try {
+            val duration = (Math.random() * 10000).toLong()
+            println("${Thread.currentThread().name} PrintQueue : Printing a Job during ${duration / 1000} seconds")
+            Thread.sleep(duration)
+        }catch (e:InterruptedException){
+            e.printStackTrace()
+        }finally {
+            queueLock.unlock()
+        }
+
+        queueLock.lock()
+
+        try {
+            val duration = (Math.random() * 10000).toLong()
+            println("${Thread.currentThread().name} PrintQueue : Printing a Job during ${duration / 1000} seconds")
+            Thread.sleep(duration)
+        }catch (e:InterruptedException){
+            e.printStackTrace()
+        }finally {
+            queueLock.unlock()
+        }
+    }
+}
+
+private class Job(private val printQueue: PrintQueue) : Runnable{
+    override fun run() {
+        println("${Thread.currentThread().name} : Going to print a document")
+        printQueue.printJob(Any())
+        println("${Thread.currentThread().name} : The document has been printed")
+    }
+}
+
+fun main(args: Array<String>) {
+    val printQueue = PrintQueue()
+    val threads = arrayOfNulls<Thread>(10)
+    for(index in 0 until threads.size){
+        threads[index] = Thread(Job(printQueue),"Thread $index")
+    }
+
+    threads.forEach {
+        it!!.start()
+        try {
+            Thread.sleep(100)
+        } catch (e: InterruptedException) {
+            e.printStackTrace()
+        }
+
+    }
+}
