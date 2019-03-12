5e34aa99b634891fe7b807b65d71823c9203ed83
Mr Medici <mr.medici@foxmail.com>
2019-03-12 22:45:12 +0800

    FindBugs use

---
+++ src/main/kotlin/chapter8/lesson6/FindBugs.kt
@@ -0,0 +1,22 @@
+package chapter8.lesson6
+
+import java.util.concurrent.TimeUnit
+import java.util.concurrent.locks.ReentrantLock
+// http://findbugs.sourceforge.net/
+private class Task(private val lock:ReentrantLock) : Runnable{
+    override fun run() {
+        lock.lock()
+        TimeUnit.SECONDS.sleep(1)
+        lock.unlock()
+    }
+}
+
+fun main(args: Array<String>) {
+    val lock = ReentrantLock()
+
+    for (index in 0 until 10){
+        val task = Task(lock)
+        val thread = Thread(task)
+        thread.run()
+    }
+}
